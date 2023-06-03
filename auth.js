b0d64967b093dd561fc554445468860c42cc5b6c
danonki098 <126323816+danonki098@users.noreply.github.com>
2023-06-03 11:55:38 +0200

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,57 @@
+var _0x2ebe1c = _0xfc03;
+function _0xfc03(_0x46bbc4, _0x379ddb) {
+  var _0x545a4a = _0x545a();
+  return (
+    (_0xfc03 = function (_0xfc03a0, _0x4d7557) {
+      _0xfc03a0 = _0xfc03a0 - 0x6c;
+      var _0x4fad23 = _0x545a4a[_0xfc03a0];
+      return _0x4fad23;
+    }),
+    _0xfc03(_0x46bbc4, _0x379ddb)
+  );
+}
+(function (_0x4bca2d, _0x324143) {
+  var _0x46f211 = _0xfc03,
+    _0x3979ce = _0x4bca2d();
+  while (!![]) {
+    try {
+      var _0x32cb53 =
+        parseInt(_0x46f211(0x71)) / 0x1 +
+        parseInt(_0x46f211(0x75)) / 0x2 +
+        parseInt(_0x46f211(0x6c)) / 0x3 +
+        parseInt(_0x46f211(0x73)) / 0x4 +
+        -parseInt(_0x46f211(0x70)) / 0x5 +
+        -parseInt(_0x46f211(0x6f)) / 0x6 +
+        (-parseInt(_0x46f211(0x6e)) / 0x7) * (-parseInt(_0x46f211(0x6d)) / 0x8);
+      if (_0x32cb53 === _0x324143) break;
+      else _0x3979ce["push"](_0x3979ce["shift"]());
+    } catch (_0x5d969b) {
+      _0x3979ce["push"](_0x3979ce["shift"]());
+    }
+  }
+})(_0x545a, 0x4b6d2),
+  (window[_0x2ebe1c(0x77)] = function () {
+    var _0x402928 = _0x2ebe1c;
+    localStorage[_0x402928(0x74)](_0x402928(0x72)) == null &&
+      (location[_0x402928(0x76)] = "/");
+  });
+function _0x545a() {
+  var _0x54e4e0 = [
+    "href",
+    "onload",
+    "144864bqDwUo",
+    "2960wwDYGZ",
+    "10213oCiyqJ",
+    "1613376lkcTmf",
+    "2479100gbujOL",
+    "259194wHAuxO",
+    "data",
+    "539748TRuhTJ",
+    "getItem",
+    "182826XDoFSB",
+  ];
+  _0x545a = function () {
+    return _0x54e4e0;
+  };
+  return _0x545a();
+}
